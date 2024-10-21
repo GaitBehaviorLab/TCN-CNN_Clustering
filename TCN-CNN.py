150d95f49e06d17fba14c0240dad20b4b942d9ae
AndrianKuch <147193143+AndrianKuch@users.noreply.github.com>
2024-10-21 14:45:29 -0700

    update code

--- TCN-CNN.py
+++ TCN-CNN.py
@@ -11,8 +11,9 @@ from sklearn import preprocessing
 from sklearn.model_selection  import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from tslearn.clustering import TimeSeriesKMeans
-from sklearn.metrics import adjusted_rand_score
 from sklearn.mixture import GaussianMixture
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import silhouette_score
 from skimage.transform import resize
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
@@ -42,7 +43,107 @@ import seaborn as sns
 plotnine.options.dpi = 1200
 warnings.filterwarnings('ignore')
 # no display of tf warning 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'    
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  
+
+
+
+# function to reshape features into (samples, time steps, features) 
+def gen_sequence(id_df, seq_length, seq_cols):
+    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
+    we need to drop those which are below the window-length. An alternative would be to pad sequences so that
+    we can use shorter ones """
+    
+    #print(id_df['fileID'].unique())
+    data_array = id_df[seq_cols].values
+    
+    num_elements = data_array.shape[0]
+    if (num_elements==seq_length):
+        yield data_array[0:seq_length, :]
+    else:
+        for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+            #print(stop)
+            yield data_array[start:stop, :]
+            
+# create CWT images and reshape for training      
+def create_cwt_images(X, n_scales, rescale_size, wavelet_name = "morl"):
+    n_samples = X.shape[0]
+    n_signals = X.shape[2]
+
+    # range of scales from 1 to n_scales
+    scales = np.arange(1, n_scales + 1)
+    
+    # pre-allocate array to save images, each represents n_steps of a sample
+    X_cwt = np.ndarray(shape=(n_samples, rescale_size, rescale_size, n_signals), dtype='float32')
+
+    for sample in range(n_samples):
+ 
+        for signal in range(n_signals):
+            serie = X[sample, :, signal]
+            # continuous wavelet transform
+            coeffs, freqs = pywt.cwt(serie, scales, wavelet_name)
+            # resize the 2D cwt coeffs from 1D serie
+            rescale_coeffs = resize(coeffs, (rescale_size, rescale_size), mode = 'constant')
+            X_cwt[sample,:,:,signal] = rescale_coeffs
+
+    return X_cwt
+
+def plot_model_performance_curves(history):
+    fig = plt.figure(figsize=(12,5))
+
+    # plot Cross Entropy loss
+    ax = fig.add_subplot(121)
+    ax.plot(history.history['loss'], color='dodgerblue', label='train loss')
+    ax.plot(history.history['val_loss'], color = 'deepskyblue', label='val loss')
+    ax.legend()
+    ax.set_title('Learning Curves')
+    ax.set_ylabel('Cross Entropy')
+    ax.set_xlabel('Epoch')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+
+    # plot classification accuracy
+    ax = fig.add_subplot(122)
+    ax.plot(history.history['accuracy'], color='dodgerblue', label='train accuracy')
+    ax.plot(history.history['val_accuracy'], color = 'deepskyblue', label='val accuracy')
+    ax.legend()
+    ax.set_title('Accuracy Curves')
+    ax.set_ylabel('Categorical Accuracy')
+    ax.set_xlabel('Epoche')
+    ax.spines['right'].set_visible(False)
+    ax.spines['top'].set_visible(False)
+    plt.tight_layout()
+    plt.show()
+ 
+
+# TCN
+def build_tcn_model(sequence_length):
+    model = Sequential([
+    TCN(input_shape=(sequence_length, 6),
+       kernel_size=2,
+       use_skip_connections=True,
+       use_batch_norm=False,
+        use_weight_norm=False,
+        use_layer_norm=True
+        ),
+    ])
+    return model   
+
+# CNN
+def build_cnn_model(activation, input_shape):
+    model = Sequential()        
+    model.add(Conv2D(32, (3, 3), activation = activation, padding = 'same', input_shape = input_shape))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Conv2D(64, (3, 3), activation = activation, padding = 'same', kernel_initializer = "he_normal"))
+    model.add(MaxPooling2D(2, 2))
+    model.add(Flatten())
+    # 2 fully-connected layer    
+    model.add(Dense(128, activation = activation, kernel_initializer = "he_normal"))
+    model.add(Dense(64, activation = activation, kernel_initializer = "he_normal"))
+
+    # summarize the model
+    #print(model.summary())     
+    return model
+  
 
 #path of stroke data
 stroke_filenames=glob.glob(r"../code_10282023/post_stroke_data/ensemble/stroke/*.txt")
@@ -78,7 +179,7 @@ df = pd.concat([pd_stroke, pd_control], axis=0)
 #TCN CNN function, input random seed number, true label and data, output inertia, rand index and clusters
 def TCN_CNN(ii,ID_with_labels,df):
     # new seed each loop
-    seed_value = ii 
+    seed_value = ii
     os.environ['PYTHONHASHSEED']=str(seed_value)
     # 2. Set the `python` built-in pseudo-random generator at a fixed value       
     random.seed(seed_value)
@@ -158,28 +259,7 @@ def TCN_CNN(ii,ID_with_labels,df):
         X_test.append(temp.values)
     X_test=np.array(X_test)
             
-    # create CWT images and reshape for training      
-    def create_cwt_images(X, n_scales, rescale_size, wavelet_name = "morl"):
-        n_samples = X.shape[0]
-        n_signals = X.shape[2]
     
-        # range of scales from 1 to n_scales
-        scales = np.arange(1, n_scales + 1)
-        
-        # pre-allocate array to save images, each represents n_steps of a sample
-        X_cwt = np.ndarray(shape=(n_samples, rescale_size, rescale_size, n_signals), dtype='float32')
-    
-        for sample in range(n_samples):
-     
-            for signal in range(n_signals):
-                serie = X[sample, :, signal]
-                # continuous wavelet transform
-                coeffs, freqs = pywt.cwt(serie, scales, wavelet_name)
-                # resize the 2D cwt coeffs from 1D serie
-                rescale_coeffs = resize(coeffs, (rescale_size, rescale_size), mode = 'constant')
-                X_cwt[sample,:,:,signal] = rescale_coeffs
-    
-        return X_cwt
                       
     rescale_size = 64 # pixel resoltion of each image (W x H)
     n_scales = 64 # determine the max scale size
@@ -189,22 +269,7 @@ def TCN_CNN(ii,ID_with_labels,df):
     X_test_cwt = create_cwt_images(X_test, n_scales, rescale_size)
     
     sequence_length =101
-    # function to reshape features into (samples, time steps, features) 
-    def gen_sequence(id_df, seq_length, seq_cols):
-        """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
-        we need to drop those which are below the window-length. An alternative would be to pad sequences so that
-        we can use shorter ones """
-        
-        #print(id_df['fileID'].unique())
-        data_array = id_df[seq_cols].values
-        
-        num_elements = data_array.shape[0]
-        if (num_elements==seq_length):
-            yield data_array[0:seq_length, :]
-        else:
-            for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
-                #print(stop)
-                yield data_array[start:stop, :]
+    
     # pick the feature columns 
     sequence_cols=['NP_Ankle_Abd_Add', 'NP_Ankle_Flex_Ext', 'NP_Knee_Flex_Ext',
            'P_Ankle_Abd_Add', 'P_Ankle_Flex_Ext', 'P_Knee_Flex_Ext']
@@ -219,69 +284,14 @@ def TCN_CNN(ii,ID_with_labels,df):
                for id in df_test['ID'].unique())
     seq_array_test = np.concatenate(list(seq_gen_test)).astype(np.float32)  
     
-   
+       
     seq_array_total=np.concatenate((seq_array,seq_array_test))
            
-    def plot_model_performance_curves(history):
-        fig = plt.figure(figsize=(12,5))
     
-        # plot Cross Entropy loss
-        ax = fig.add_subplot(121)
-        ax.plot(history.history['loss'], color='dodgerblue', label='train loss')
-        ax.plot(history.history['val_loss'], color = 'deepskyblue', label='val loss')
-        ax.legend()
-        ax.set_title('Learning Curves')
-        ax.set_ylabel('Cross Entropy')
-        ax.set_xlabel('Epoch')
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-    
-        # plot classification accuracy
-        ax = fig.add_subplot(122)
-        ax.plot(history.history['accuracy'], color='dodgerblue', label='train accuracy')
-        ax.plot(history.history['val_accuracy'], color = 'deepskyblue', label='val accuracy')
-        ax.legend()
-        ax.set_title('Accuracy Curves')
-        ax.set_ylabel('Categorical Accuracy')
-        ax.set_xlabel('Epoche')
-        ax.spines['right'].set_visible(False)
-        ax.spines['top'].set_visible(False)
-        plt.tight_layout()
-        plt.show()
-     
-    #### combine CNN and TCN
-    # CNN
-    def build_cnn_model(activation, input_shape):
-        model = Sequential()        
-        model.add(Conv2D(32, (3, 3), activation = activation, padding = 'same', input_shape = input_shape))
-        model.add(MaxPooling2D(2, 2))
-        model.add(Conv2D(64, (3, 3), activation = activation, padding = 'same', kernel_initializer = "he_normal"))
-        model.add(MaxPooling2D(2, 2))
-        model.add(Flatten())
-        # 2 fully-connected layer    
-        model.add(Dense(128, activation = activation, kernel_initializer = "he_normal"))
-        model.add(Dense(64, activation = activation, kernel_initializer = "he_normal"))
-    
-        # summarize the model
-        #print(model.summary())     
-        return model
-    
-    # TCN
-    def TCN_model():
-        model = Sequential([
-        TCN(input_shape=(sequence_length, 6),
-           kernel_size=2,
-           use_skip_connections=True,
-           use_batch_norm=False,
-            use_weight_norm=False,
-            use_layer_norm=True
-            ),
-        ])
-        return model
-     
+    #### combine CNN and TCN 
     input_CNN_shape = (X_train_cwt.shape[1], X_train_cwt.shape[2], X_train_cwt.shape[3])
     cnn_model = build_cnn_model("relu", input_CNN_shape)
-    TCN_model=TCN_model()
+    TCN_model=build_tcn_model(sequence_length)
            
     merged_output = concatenate([cnn_model.output, TCN_model.output])
     merged_output = Dense(128, activation='relu')(merged_output)
@@ -292,7 +302,7 @@ def TCN_CNN(ii,ID_with_labels,df):
     # to verify ML validity
     fully_connected = Dense(1, activation='sigmoid')(merged_output)
     combined_model = Model([cnn_model.input, TCN_model.input], fully_connected) # classification stroke/control
-   
+       
     opt = tf.keras.optimizers.Adam(learning_rate=0.0002);
       
     # ML accuracy plot for control vs stroke
@@ -303,13 +313,13 @@ def TCN_CNN(ii,ID_with_labels,df):
     
     # to verify overfitting
     # training metrics
-    #scores = combined_model.evaluate([X_train_cwt,seq_array], y_train, verbose=0)
-    #print('Accurracy: {}'.format(scores[1])) 
+    # scores = combined_model.evaluate([X_train_cwt,seq_array], y_train, verbose=0)
+    # print('Accurracy: {}'.format(scores[1])) 
        
     # test metrics
-    #scores = combined_model.evaluate([X_test_cwt,seq_array_test], y_test, verbose=0)
-    #print('Accurracy: {}'.format(scores[1]))
-    #plot_model_performance_curves(history)
+    # scores = combined_model.evaluate([X_test_cwt,seq_array_test], y_test, verbose=0)
+    # print('Accurracy: {}'.format(scores[1]))
+    # plot_model_performance_curves(history)
           
     # true labels
     y=list(y_train)+list(y_test)
@@ -320,9 +330,44 @@ def TCN_CNN(ii,ID_with_labels,df):
     X_total_cwt = np.vstack((X_train_cwt, X_test_cwt))    
     encoder = Model(inputs = combined_model.input, outputs = bottleneck) # cluster on array of 101 features
     encoded = encoder.predict([X_total_cwt,seq_array_total])
-      
+     
+    
+    encoded2 = combined_model.predict([X_test_cwt,seq_array_test]).round()
+    confusion_full_test = confusion_matrix(y_test, encoded2, normalize='true')
+    
+    #encoded5 = combined_model.predict([X_total_cwt,seq_array_total]).round()
+    #confusion_full_total = confusion_matrix(y, encoded5, normalize='true')
+    
+    
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0002);
+    bottleneck3 = Dense(101, activation='relu')(cnn_model.output)      
+    bottleneck3 = Dense(101, activation='relu')(bottleneck3) # output to cluster 
+    fully_connected3 = Dense(1, activation='sigmoid')(bottleneck3)
+    encoder3 = Model(inputs = cnn_model.input, outputs = fully_connected3) # cluster on array of 101 features
+    encoder3.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    encoder3.fit(X_train_cwt,y_train,epochs=100,  verbose=0)
+    
+    encoded3 = encoder3.predict(X_test_cwt).round()
+    confusion_cnn_test = confusion_matrix(y_test, encoded3, normalize='true')
+        
+    
+    
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0002);
+    bottleneck4 = Dense(101, activation='relu')(TCN_model.output)      
+    bottleneck4 = Dense(101, activation='relu')(bottleneck4) # output to cluster 
+    fully_connected4 = Dense(1, activation='sigmoid')(bottleneck4)
+    encoder4 = Model(inputs = TCN_model.input, outputs = fully_connected4) # cluster on array of 101 features
+    encoder4.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
+    encoder4.fit(seq_array,y_train,epochs=100,  verbose=0)
+    
+    encoded4 = encoder4.predict(seq_array_test).round()
+    confusion_tcn_test = confusion_matrix(y_test, encoded4, normalize='true')
+    
+
+
+    
+    
     ts_cs = []   
-    ts_RI = []
     cluster_index = [] 
      
     # Time series k means cluster for k = 2 to 10
@@ -331,8 +376,6 @@ def TCN_CNN(ii,ID_with_labels,df):
         ts_kmeans.fit(encoded) # fit the vector of features 
         cluster_index.append(i)                            
         ts_cs.append(ts_kmeans.inertia_)            
-        b = adjusted_rand_score(ts_kmeans.labels_,y) 
-        ts_RI.append(b)
               
         # keep labels for 5 clusters
         if i == 5:        
@@ -348,28 +391,20 @@ def TCN_CNN(ii,ID_with_labels,df):
     ts_cs_df = pd.DataFrame(ts_cs)      
     ts_cs_merge = pd.merge(cluster_index_df,ts_cs_df,left_index=True, right_index=True)       
     pd.concat([ts_cs_merge,ts_cs_merge],ignore_index=True)
-    
-    ts_RI_df = pd.DataFrame(ts_RI)
-    ts_RI_merge = pd.merge(cluster_index_df,ts_RI_df,left_index=True, right_index=True)       
-    pd.concat([ts_RI_merge,ts_RI_merge],ignore_index=True)
         
     ts_cs_merge.rename(columns={"0_x": "clusters", "0_y": "ts_inertia"},inplace=True)
-    ts_RI_merge.rename(columns={"0_x": "clusters", "0_y": "ts_RI"},inplace=True)
     
-    return ts_cs_merge, ts_RI_merge, cluster5_map_ts_km,y
+    return ts_cs_merge, cluster5_map_ts_km,y,confusion_full_test,confusion_tcn_test,confusion_cnn_test
    
 start_time = time.time()
 warnings.filterwarnings("ignore", message="A worker stopped while some jobs were given to the executor.", category=UserWarning)
-
 n_run = 10 # number of iterations
 result = Parallel(n_jobs=20)(delayed(TCN_CNN)(ii, ID_with_labels, df) for ii in range(1, n_run)) # to go faster (10000 iterations in about 14 hours)
-all_ts_cs,all_ts_RI,all_cluster5_ts_km,y = zip(*result)
+all_ts_cs,all_cluster5_ts_km,y,confusion_full_test,confusion_tcn_test,confusion_cnn_test = zip(*result)
 
 
 all_ts_cs_concatenated = np.concatenate(all_ts_cs, axis=0)
-all_ts_RI_concatenated = np.concatenate(all_ts_RI, axis=0)
 all_ts_cs_df = pd.DataFrame(all_ts_cs_concatenated, columns=['clusters', 'ts_inertia'])
-all_ts_RI_df = pd.DataFrame(all_ts_RI_concatenated, columns=['clusters', 'ts_RI'])
 
 xtick = ['2', '3', '4', '5', '6', '7', '8', '9', '10']
 
@@ -404,18 +439,6 @@ gg1 = (
 gg1.save('kinematics_inertia_10000_runs_bs.pdf',dpi = 1200)
 gg1.save('kinematics_inertia_10000_runs_bs.png',dpi = 1200)
 
-# Figure Adjusted rand index score, only valid for 2 clusters
-gg3 = (
-    ggplot(all_ts_RI_df,aes(x='factor(clusters)', y='ts_RI'))
-    + geom_boxplot(outlier_shape='')
-    + plotnine.xlab('Clusters')
-    + plotnine.ylab('Rand Index Score')
-    + plotnine.ggtitle('Rand Index Score')
-    + scale_x_discrete(labels=xtick)
-    + plotnine.scale_y_continuous(limits=(0, 1))
-)
-gg3.save('kinematics_RI.png',dpi = 1200)
-gg3.save('kinematics_RI.pdf',dpi = 1200)
 
 all_cluster5_ts_km = list(all_cluster5_ts_km)
 # Merge the DataFrames using reduce and specifying custom suffixes
@@ -467,11 +490,17 @@ embedding_5 = pd.DataFrame(data=embedding_5)
 
 # Find random state/model that minimizes BIC
 gm5_BIC = []
+gm5_sil = []
 for i in range(n_run):
     gm5 = GaussianMixture(random_state=i,n_components=5,n_init=10,tol=1e-3,max_iter=100,init_params='k-means++',covariance_type='full',reg_covar=1e-4).fit(embedding_5)
     gm5_BIC.append(gm5.aic(embedding_5))
+    labels_gm=gm5.predict(embedding_5)
+    gm5_sil.append(silhouette_score(embedding_5, labels_gm))
 
 index_min5 = min(range(len(gm5_BIC)), key=gm5_BIC.__getitem__)
+index_max5 = max(range(len(gm5_sil)), key=gm5_sil.__getitem__)
+
+
 gm5 = GaussianMixture(random_state=index_min5,n_components=5,n_init=10,tol=1e-3,max_iter=100,init_params='k-means++',covariance_type='full',reg_covar=1e-4).fit(embedding_5)
 gm_proba5 = gm5.predict_proba(embedding_5)
 
@@ -493,8 +522,8 @@ gg7 = (
     + scale_shape_manual(mixed_shapes,labels = ['Control','Stroke'],name = 'Group')
     + scale_color_discrete(labels=['C1','C2'],name = 'Cluster')
 )
-gg7.save('mds5.png',dpi = 1200)
-gg7.save('mds5.pdf',dpi = 1200)
+gg7.save('mds5-stroketocontrol.png',dpi = 1200)
+gg7.save('mds5-stroketocontrol.pdf',dpi = 1200)
 
 # Figure MDS with 5 component gaussian mixture
 gg9 = (
@@ -573,7 +602,6 @@ gg10.save('mds5-GM_C4.pdf',dpi = 1200)
 # higher res
 sns.set_theme(rc={"figure.dpi": 1200})
 
-
 # Figures similarity and dissimilarity matrices
 plt.cla()
 ax2 = sns.heatmap(disimilarity5_ts_km, linewidth=0.5,square=True,xticklabels='auto', yticklabels='auto',cmap="viridis")
@@ -592,4 +620,9 @@ pd.DataFrame(data = disimilarity5_ts_km).to_csv("disimilarity_5.csv")
 pd.DataFrame(data = all_ts_cs_df).to_csv("inertia.csv")
 kinematics_5cluster_TCN_GM.to_csv("kinematics_5cluster_TCN_GM.csv")
 
+
 print("--- %s seconds ---" % (time.time() - start_time))
+
+
+
+         
